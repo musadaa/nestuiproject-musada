266593edc36994b3fcf7ab8362d4c3f5f9628d1e
musadaa <musadaopr@gmail.com>
2023-02-12 13:35:33 +0700

    updated case

--- cypress/e2e/3-learning/test.cy.js
+++ cypress/e2e/3-learning/test.cy.js
@@ -1,7 +1,5 @@
 /// <reference types="cypress" />
 
-const { contains } = require("cypress/types/jquery");
-
 
 describe('Add Customer', function(){
     it('Valid Add Customer', () => {
@@ -9,7 +7,20 @@ describe('Add Customer', function(){
         cy.contains('Bank Manager Login').click();
         cy.get('[ng-class="btnClass1"]').click();
         cy.get(':nth-child(1) > .form-control').type('Harry');
-        cy.get(':nth-child(2) > .form-control').type('Rabu');
+        cy.get(':nth-child(2) > .form-control').type('Kamis');
         cy.get(':nth-child(3) > .form-control').type('65432');
+        cy.get('form').submit();
+
+    }),
+
+    it('Exist Customer', () => {
+        cy.visit('https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login');
+        cy.contains('Bank Manager Login').click();
+        cy.get('[ng-class="btnClass1"]').click();
+        cy.get(':nth-child(1) > .form-control').type('Harry');
+        cy.get(':nth-child(2) > .form-control').type('Potter');
+        cy.get(':nth-child(3) > .form-control').type('61829182');
+        cy.get('form').submit();
+
     })
 })
